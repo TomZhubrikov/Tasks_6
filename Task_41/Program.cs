043d153ed25fbe90950e37e2ade7ba197feee8bf
Tom <artzhubrikov@mail.ru>
2022-12-11 13:18:05 +0300

    Comp

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,6 +1,15 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 // 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+// 1, -7, 567, 89, 223 -> 4
 
-Console.WriteLine("Hello, World!");
+bool F = true;
+int n, count = 0;
+while (F)
+{
+    Console.Write("('666' for exit) Enter a number -> ");
+    n = int.Parse(Console.ReadLine());
+    if (n == 666) F = false;
+    else if (n > 0) count++;
+}
+Console.WriteLine($"Entered {count} numbers > 0!");
--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -2,4 +2,17 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hi! This is a program that will find the intersection point of two straight lines given by tasks y = k1 * x + b1, y = k2 * x + b2;");
+Console.Write("Enter value k1 -> ");
+float k1 = float.Parse(Console.ReadLine());
+Console.Write("Enter value b1 -> ");
+float b1 = float.Parse(Console.ReadLine());
+Console.Write("Enter value k2 -> ");
+float k2 = float.Parse(Console.ReadLine());
+Console.Write("Enter value b2 -> ");
+float b2 = float.Parse(Console.ReadLine());
+
+// x0 and y0 - intersection point coordinates.
+float x0 = (b2 - b1) / (k1 - k2);
+float y0 = k2 * x0 + b2;
+Console.WriteLine($"({x0}; {y0})");
